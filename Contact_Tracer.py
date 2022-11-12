7ac608a8e9c3e22702a63b093e556fa407dba430
unknown <Christiandevega12@gmail.com>
2022-11-12 11:25:02 +0800

    -added input feature

--- Contact_Tracer.py
+++ Contact_Tracer.py
@@ -2,3 +2,11 @@ print("Menu:")
 print("1-> add an item")
 print("2-> search an item")
 print("3-> Exit(y/n)")
+
+user_opt=int(input("Select an item: "))
+if user_opt==1:
+    print("You have chosen the option to add an item")
+if user_opt==2:
+    print("You have chosen the option to search for an item")
+if user_opt==3:
+    print("Do you wish to exit?")
