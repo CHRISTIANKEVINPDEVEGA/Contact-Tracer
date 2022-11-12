f47a45d769f6be215185f2a364e101eab78fbe9c
unknown <Christiandevega12@gmail.com>
2022-11-12 14:53:21 +0800

    overhaul the option1 to support nested dictionary, using array

--- Contact_Tracer.py
+++ Contact_Tracer.py
@@ -1,51 +1,28 @@
-#personaldata={"Name":"","Age":"","Address":"","Phone Number":""}
+Contact_Tracing_DataBase={}
 
-#while True:
-#    print("Menu:")
-#    print("1-> add an item")
-#    print("2-> Search")
-#    print("3-> Exit(y/n)")
-#    user_opt=int(input("Select an item: "))
-#    if user_opt==1:
-#        print("You have chosen the option to add an item")
-#        Name_=input("Enter your Name: ")
-#        personaldata["Name"]=Name_
-#       Age_=input("Enter your Age: ")
-#        personaldata["Age"]=Age_
-#        Address_=input("Enter your Address: ")
-#        personaldata["Address"]=Address_
-#        Phone_Number_=(input("Enter your Phone Number: "))
-#        personaldata["Phone Number"]=Phone_Number_
-#    elif user_opt==2:
-#        print("You have chosen the option to search")
-#        print("Name: "+ personaldata.get("Name"))
-#        print("Age: "+ personaldata.get("Age"))
-#        print("Address: "+ personaldata.get("Address"))
-#        print("Phone Number: "+ personaldata.get("Phone Number"))
-
-#   elif user_opt==3:
-#        user_opt3=input("Do you wish to exit? ")
-#       if user_opt3=="yes":
-#          break
-#       if user_opt3=="no":
-#           continue
-database={}
-
-for i in range(10):
-    personalinfo=[]
-    user_input=input("put name: ")
-    personalinfo.append(user_input)
-    user_input=input("Age: ")
-    personalinfo.append(user_input)
-    user_input=input("Adress: ")
-    personalinfo.append(user_input)
-    user_input=input("Phone Number: ")
-    personalinfo.append(user_input)
-    database[personalinfo[0]]={"Name":personalinfo[0],"Age":personalinfo[1],"Address":personalinfo[2],"Phone Number":personalinfo[3]}
-    break
-search=input("name: ")
-for key in database:
-    if search == key:
-        print(database[search])
-
+while True:
+    print("Menu:")
+    print("1-> add an item")
+    print("2-> Search")
+    print("3-> Exit(y/n)")
+    user_opt=int(input("Select an item: "))
+    if user_opt==1:
+        personalinfo=[]
+        user_input=input("put name: ")
+        personalinfo.append(user_input)
+        user_input=input("Age: ")
+        personalinfo.append(user_input)
+        user_input=input("Adress: ")
+        personalinfo.append(user_input)
+        user_input=input("Phone Number: ")
+        personalinfo.append(user_input)
+        Contact_Tracing_DataBase[personalinfo[0]]={"Name":personalinfo[0],"Age":personalinfo[1],"Address":personalinfo[2],"Phone Number":personalinfo[3]}
     
+    elif user_opt==3:
+        user_opt3=input("Do you wish to exit? ")
+        if user_opt3=="yes":
+          break
+        if user_opt3=="no":
+           continue
+
+
