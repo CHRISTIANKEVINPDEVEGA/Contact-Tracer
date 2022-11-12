be57a92911d93ee4a0210e0b86e89d0e9240b0fb
unknown <Christiandevega12@gmail.com>
2022-11-12 12:03:09 +0800

    -added option 2 and 3

--- Contact_Tracer.py
+++ Contact_Tracer.py
@@ -1,29 +1,35 @@
-print("Menu:")
-print("1-> add an item")
-print("2-> search an item")
-print("3-> Exit(y/n)")
-
 personaldata={
     "Name":"",
     "Age":"",
     "Address":"",
     "Phone Number":""
 }
-
-user_opt=int(input("Select an item: "))
-if user_opt==1:
-    print("You have chosen the option to add an item")
-    Name_=input("Enter your Name: ")
-    personaldata["Name"]=Name_
-    Age_=input("Enter your Age: ")
-    personaldata["Age"]=Age_
-    Address_=input("Enter your Address: ")
-    personaldata["Address"]=Address_
-    Phone_Number_=input("Enter your Phone Number: ")
-    personaldata["Phone Number"]:Phone_Number_
-    print(personaldata)
-if user_opt==2:
-    print("You have chosen the option to search for an item")
-
-if user_opt==3:
-    print("Do you wish to exit?")
+while True:
+    print("Menu:")
+    print("1-> add an item")
+    print("2-> Search")
+    print("3-> Exit(y/n)")
+    user_opt=int(input("Select an item: "))
+    if user_opt==1:
+        print("You have chosen the option to add an item")
+        Name_=input("Enter your Name: ")
+        personaldata["Name"]=Name_
+        Age_=input("Enter your Age: ")
+        personaldata["Age"]=Age_
+        Address_=input("Enter your Address: ")
+        personaldata["Address"]=Address_
+        Phone_Number_=(input("Enter your Phone Number: "))
+        personaldata["Phone Number"]=Phone_Number_
+    elif user_opt==2:
+        print("You have chosen the option to search")
+        print("Name: "+ personaldata.get("Name"))
+        print("Age: "+ personaldata.get("Age"))
+        print("Address: "+ personaldata.get("Address"))
+        print("Phone Number: "+ personaldata.get("Phone Number"))
+        
+    elif user_opt==3:
+        user_opt3=input("Do you wish to exit? ")
+        if user_opt3=="yes":
+            break
+        if user_opt3=="no":
+            continue
